866ce88379bc335b4df70cf093475417720087ec
Andrew Dunbar <hippytrail@gmail.com>
2022-10-04 03:24:21 +0800

    filetype checks in the dir

--- src/main.zig
+++ src/main.zig
@@ -15,11 +15,11 @@ pub fn main() !void {
     const al = std.heap.c_allocator;
 
     const clr = try processCommandline(al);
-    const opts = clr.opts;
+    const opts = clr.opts; _ = opts;
     const args = clr.args;
     
     for (args) |arg| {
-        try processFilename(al, arg);
+        try processFilename(std.fs.cwd(), arg);
     }
     
     al.free(args);
@@ -52,10 +52,10 @@ fn processCommandline(al: std.mem.Allocator) !CommandLineResults {
                 const sw = arg[1..len];
                 if (std.mem.eql(u8, sw, "d")) {
                     opts.d = true;
-                    std.debug.print("-{s} = follow dot\n", .{sw});
+                    //std.debug.print("-{s} = follow dot\n", .{sw});
                 } else if (std.mem.eql(u8, sw, "v")) {
                     opts.v = true;
-                    std.debug.print("-{s} = verbose\n", .{sw});
+                    //std.debug.print("-{s} = verbose\n", .{sw});
                 } else {
                     std.debug.print("* unknown switch: '{s}'\n", .{sw});
                 }
@@ -68,13 +68,12 @@ fn processCommandline(al: std.mem.Allocator) !CommandLineResults {
                 if (std.mem.eql(u8, key, "m")) {
                     const md = try std.fmt.parseInt(i8, val, 10);
                     opts.maxdepth = md;
-                    std.debug.print("-m = max recursion depth: {}\n", .{md});
+                    //std.debug.print("-m = max recursion depth: {}\n", .{md});
                 } else {
                     std.debug.print("* unknown switch: {s} = {s}\n", .{key, val});
                 }
             }
         } else {
-            //std.debug.print("arg: {s}\n", .{arg});
             // convert the argument from c-style zero-terminated strings to native zig slices
             try arglist.append(arg[0..len]);
         }
@@ -125,32 +124,48 @@ fn processCommandline(al: std.mem.Allocator) !CommandLineResults {
     // std.debug.print("\n", .{});
 }
 
-fn processFilename(al: std.mem.Allocator, path:[]const u8) !void {
-    var dir = std.fs.cwd().openIterableDir(path, .{}) catch |err| {
-        switch (err) {
-            error.NotDir => {
-                std.debug.print("* not a directory: '{s}'\n", .{path});
-            },
-            error.FileNotFound => {
-                std.debug.print("* doesn't exist: '{s}'\n", .{path});
-            },
-            error.AccessDenied => {
-                std.debug.print("* access denied: '{s}'\n", .{path});
-            },
+fn processFilename(cwd: std.fs.Dir, path:[]const u8) !void {
+    var dir = cwd.openIterableDir(path, .{}) catch |err| {
+        var msg: ?[]const u8 = switch (err) {
+            error.NotDir => "not a directory",
+            error.FileNotFound => "doesn't exist",
+            error.AccessDenied => "access denied",
             else => {
                 std.debug.print("** BANG: {s} {any}\n", .{path, err});
                 @panic("unexpected error trying to open a directory");
-            }
-        }
+            },
+        };
+        std.debug.print("* {s}: '{s}'\n", .{msg.?, path});
         return;
     };
-
-    std.debug.print("'{s}' -> dir result: {any}\n", .{path, dir});
+    defer dir.close();
 
     var diri = dir.iterate();
 
-    while (try diri.next()) |necks| {
-        std.debug.print("  iterated: {s} ({any})\n", .{necks.name, necks.kind});
+    while (try diri.next()) |entry| {
+        // check for zero-length files and unopenable files
+        var kindSymbol: ?u8 = null;
+        if (entry.kind == .File) blk: {
+            const f: std.fs.File = dir.dir.openFile(entry.name, .{}) catch {
+                kindSymbol = '!';
+                break :blk;
+            };
+            defer f.close();
+
+            if ((try f.stat()).size == 0) {
+                kindSymbol = '0';
+            } else {
+                kindSymbol = ' ';
+            }
+        } else {
+            kindSymbol = switch (entry.kind) {
+                .Directory => '/',
+                .SymLink => '~',
+                else => '?',
+            };
+        }
+
+        std.debug.print("{c} {s}\n", .{kindSymbol.?, entry.name});
     }
 
     // var walker = try dir.walk(al);
@@ -174,6 +189,4 @@ fn processFilename(al: std.mem.Allocator, path:[]const u8) !void {
     //         }
     //     }
     // }
-
-    dir.close();
 }
